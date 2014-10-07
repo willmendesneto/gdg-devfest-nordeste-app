1ad28611b5c32fa14c4582c22d1c78c14402fb58
willmendesneto <willmendesneto@gmail.com>
2014-10-06 22:40:50 -0300

    fix openInBrowser directive

--- www/js/directives/OpenInBrowser.js
+++ www/js/directives/OpenInBrowser.js
@@ -6,16 +6,18 @@ angular.module('gdgApp')
       scope: {
         link: '='
       },
-      link: function($scope, $el) {
-        $el.on('click', function(){
-          window.open($scope.link, '_blank', 'location=yes');
-          return false;
-        });
+      link: {
+        post: function($scope, $el, $attrs) {
+          $el.bind('click', function(){
+            window.open($scope.link, '_system', 'location=yes');
+            return false;
+          });
 
-        $scope.$on('$destroy', function(){
-          $el.unbind('click');
-          $scope.$destroy();
-        });
+          $scope.$on('$destroy', function(){
+            $el.unbind('click');
+            $scope.$destroy();
+          });
+        }
       }
     }
   });
